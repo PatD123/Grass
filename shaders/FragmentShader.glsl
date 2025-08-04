5ada3b17b34bbff47cf17aff7ff5fe3a682f522a
Patrick Dai <patrickdai917@gmail.com>
2025-08-03 17:11:36 -0700

    Reverted grass color.

--- shaders/FragmentShader.glsl
+++ shaders/FragmentShader.glsl
@@ -13,7 +13,7 @@ float ease_in_quartic(float x) {
 void main()
 {
     vec3 base_color = vec3(0.05,0.2,0.01);
-	vec3 tip_color = vec3(0.5,0.7,0.1);
+	vec3 tip_color = vec3(0.5,0.55,0.1);
 	FragColor = vec4(mix(base_color, tip_color, ease_in_quartic(RelativeHeight)), 1.0);
 	FragColor *= mix(0.1, 1.0, RelativeHeight*RelativeHeight);
 
